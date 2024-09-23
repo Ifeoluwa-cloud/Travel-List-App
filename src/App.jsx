07e683e3710ee99f478b6a3aedffba0ba69bd14f
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-09-23 13:16:13 +0100

    Created App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-export default function App() {}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function Logo() {
-  return <h1>Far Away</h1>
+
+
+
+export default function App() {
+  return <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+  </div>
 }
 
-function Form() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>
+}
 
-function PackingList() {}
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your 😍 trip?</h3>
+  </div>
+}
 
-function Stats() {}
+function PackingList() {
+  return (
+    <div className="list">
+          <ul>
+            {initialItems.map((item) => (
+              <Item key={item.id} item={item} />
+            ))}
+          </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+  <li>{item.description}</li>
+  );
+} 
+
+function Stats() {
+  return <footer className="stats">
+    <em>💼 You have X items on your, and you already packed X (X%)</em>
+  </footer>
+}
