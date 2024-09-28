d75d0ae2af9c2608965ce3ead3b235fbdeaaa12f
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-09-28 17:38:16 +0100

    Form headline updated

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function Logo() {
 
 function Form() {
   return <form className="add-form">
-    <h3>What do you need for your adventurous😍 trip?</h3>
+    <h3>What do you need for your 😍 trip?</h3>
     <select name="" id="">
       {Array.from({length:20}, (_, i) => i + 1).
       map(num=><option value={num} key={num}>{num}</option>)}
