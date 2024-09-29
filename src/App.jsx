c78007b53bd951023bf7ca8e6b8def9bd3f9136d
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-09-29 20:42:31 +0100

    updated array list

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function Form() {
   return <form className="add-form">
     <h3>What do you need for your 😍 trip?</h3>
     <select name="" id="">
-      {Array.from({length:20}, (_, i) => i + 1).
+      {Array.from({ length:22 }, (_, i) => i + 1).
       map(num=><option value={num} key={num}>{num}</option>)}
     </select>
     <input type="text" placeholder="Item..." />
