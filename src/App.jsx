cda7ec3ec1b0501deeab3005f6b1a145095549b8
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-10-01 14:05:05 +0100

    Updated the handleSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,16 +20,25 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1)
   }
 
-
-  return ( 
+    return ( 
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="" id="">
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length:22 }, (_, i) => i + 1).
         map((num) => (
           <option value={num} key={num}>
@@ -35,7 +46,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input 
+      type="text" 
+      placeholder="Item..." 
+      value={description} 
+      onChange={(e)=> setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
