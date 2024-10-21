50df0ed58aae99576369360540f5294e082a4198
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2024-10-21 22:28:11 +0100

    Refractor the Form Component

--- src/Form.jsx
+++ src/Form.jsx
@@ -45,7 +45,9 @@ export default function Form({ onAddItems }) {
             value={description} 
             onChange={(e)=> setDescription(e.target.value)}
         />
-        <button>Add</button>
+        <button>
+          Add
+        </button>
       </form>
     );
   }
